47d296c3341a282623087fa860c69fc807ebe398
pyatkov <pyatkov@media-saturn.com>
2017-09-04 18:51:14 +0300

    Added script for running DP.exe in Import mode allowing to bypass user input.

---
+++ Start-ImportViaDP.ps1
@@ -0,0 +1,18 @@
+﻿$dataFolder = $args[0]
+$databaseName = $args[1]
+
+Write-Host "Data folder is ($dataFolder)" -ForegroundColor Cyan
+
+Write-Host "Database name is ($databaseName)" -ForegroundColor Cyan
+
+$psi = New-Object 'System.Diagnostics.ProcessStartInfo'
+$psi.UseShellExecute = $false
+$psi.RedirectStandardInput = $true
+$psi.FileName = 'dp.exe'
+$psi.Arguments = @('IMPORT', $dataFolder, $databaseName)
+
+$process = New-Object 'System.Diagnostics.Process'
+$process.StartInfo = $psi
+$process.Start()
+
+return $process
